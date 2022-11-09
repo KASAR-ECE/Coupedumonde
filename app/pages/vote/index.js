5cd0e85385bf218be09179af48dce3951411a6ad
romain-sen <58177664+romain-sen@users.noreply.github.com>
2022-11-09 01:18:59 +0100

    basic layout vote page

--- app/pages/vote/index.js
+++ app/pages/vote/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
+
+const db = [
+    {
+        equipe1: "France",
+        equipe2: "Allemagne",
+    },
+    {
+        equipe1: "Bresil",
+        equipe2: "Portugal",
+    },
+    {
+        equipe1: "Japon",
+        equipe2: "Corée du Nord",
+    },
+]
 
 const votePage = () => {
+    const voteHandler = (equipe) => {
+        console.log("Vote pour l'équipe " + equipe)
+    }
+
     return (
-        <div>votePage</div>
+        <div>
+            {db.map((match, index) => {
+                return (
+                    <div key={index} className="p-6 max-w-s rounded-lg border border-gray-200 shadow-md bg-kasar1">
+                        <div className="max-w-md">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white ">{match.equipe1} - VS - {match.equipe2}</h5>
+                            <div className="flex justify-between ">
+                                <button onClick={() => { voteHandler(match.equipe1) }}
+                                    className="text-kasar1 font-bold bg-white border border-kasar2 rounded-lg p-2 hover:bg-kasar3">{match.equipe1}</button>
+                                <button onClick={() => { voteHandler(match.equipe2) }}
+                                    className="text-kasar1 font-bold bg-white border border-kasar2 rounded-lg p-2 hover:bg-kasar3">{match.equipe2}</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
