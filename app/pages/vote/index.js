b824d4c0679e9b1e183cada0e1eaf247660bd8eb
romain-sen <58177664+romain-sen@users.noreply.github.com>
2022-11-11 11:35:08 +0100

    front end votes

--- app/pages/vote/index.js
+++ app/pages/vote/index.js
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import ButtonQuickVote from "../../components/votes/ButtonQuickVote"
+import VoteCards from "../../components/votes/VoteCards"
 
 const db = [
     {
@@ -16,32 +19,14 @@ const db = [
 ]
 
 const votePage = () => {
-    const voteHandler = (equipe) => {
-        console.log("Vote pour l'équipe " + equipe)
-    }
+
 
     return (
         <div>
             {db.map((match, index) => {
                 return (
-                    <div key={index} className="p-6 max-w-s rounded-lg border border-gray-200 shadow-md bg-kasar1">
-                        <div className="max-w-md">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white ">{match.equipe1} - VS - {match.equipe2}</h5>
-                            <div className="flex justify-between ">
-                                <div>
-                                    <button onClick={() => { voteHandler(match.equipe1) }}
-                                        className="text-kasar1 font-bold bg-white border border-kasar2 rounded-lg p-2
-                                        hover:bg-kasar3">{match.equipe1}</button>
-                                    <input type="number" />
-                                </div>
-                                <div>
-                                    <button onClick={() => { voteHandler(match.equipe2) }}
-                                        className="text-kasar1 font-bold bg-white border border-kasar2 rounded-lg p-2
-                                    hover:bg-kasar3">{match.equipe2}</button>
-                                    <input type="number" />
-                                </div>
-                            </div>
-                        </div>
+                    <div className="p-2" key={index}>
+                        <VoteCards match={match} id={index} key={index} />
                     </div>
                 )
             })}
