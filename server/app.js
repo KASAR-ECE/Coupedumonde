3e70dfb12b740d6586dd1bf48955e38ea45a75c2
romain-sen <58177664+romain-sen@users.noreply.github.com>
2022-11-11 12:43:44 +0100

    get games from api

--- server/app.js
+++ server/app.js
@@ -3,16 +3,11 @@ const express = require('express')
 const cors = require("cors");
 
 
-
-
-
-
-
-
-
-
-
 const app = express();
+
+//Allow next js app running on localhost 3000 to get acces to the API
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 
 
@@ -20,13 +15,10 @@ app.use(express.json());
 const signup = require('./routes/signup');
 const signin = require('./routes/signin');
 
+const games = require('./routes/games')
 
-
-
-
-
-
-app.use('/signup',signup);
-app.use('/signin',signin);
+app.use('/signup', signup);
+app.use('/signin', signin);
+app.use('/games', games);
 
 module.exports = app;
--- server/db.js
+++ server/db.js
@@ -1,8 +1,6 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-
-
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
