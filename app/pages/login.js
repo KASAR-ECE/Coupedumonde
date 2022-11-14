fe954206cb4fef3342d44ea33f020dd7865f7ec7
martin <martin.labat92@gmail.com>
2022-11-14 19:18:13 +0100

    fix: style & node modules missing
    Signed-off-by: martin <martin.labat92@gmail.com>

--- app/pages/login.js
+++ app/pages/login.js
@@ -43,27 +43,27 @@ const Login = () => {
   }
   return (
 
-    <div class="grid place-items-center">
+    <div className="grid place-items-center">
       <Head>
       <link rel="icon" href="/favicon.ico" />
       <title>Login</title>
       </Head>
       
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-    <label class="block text-gray-700 text-sm font-bold mb-2" >
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+    <label className="block text-gray-700 text-sm font-bold mb-2" >
         Username
         </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         name="username"
         type="username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         required="required"
       />
-      <label class="mt-4 block text-gray-700 text-sm font-bold mb-2" >
+      <label className="mt-4 block text-gray-700 text-sm font-bold mb-2" >
         Password
       </label>
-      <input class=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      <input className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         name="password"
         type="password"
         value={password}
@@ -75,7 +75,7 @@ const Login = () => {
       <div className='flex justify-evenly mt-4'>
 
       
-      <input type="submit" value="Submit" class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 text-white font-bold py-2 px-4 rounded focus:outline-none " />
+      <input type="submit" value="Submit" className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 text-white font-bold py-2 px-4 rounded focus:outline-none " />
       <p className='mt-auto text-sm mb-auto text-gray-700'><Link href="/signup">No account ?</Link></p>
       </div>
     </form>
