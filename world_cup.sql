eac0899862aa76d21846360953ba8eaa2b3de7bc
M.Couraud <mathias.couraud@gmail.com>
2022-11-19 15:42:53 +0100

    New database + game dates + ranking + handle connection (#18)
    * add cotes to database + feat : games time
    
    * fix : handledisconnexion
    
    * fix : cotes have draw
    
    * Order games by date + cotes
    
    * Ranking back + front
    
    * feat: title to ranking page
    
    * fix: title
    
    * fix conflicts
    
    * fix: remove unused href
    
    Co-authored-by: martin <martin.labat92@gmail.com>

--- world_cup.sql
+++ world_cup.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 172.17.0.1:3306
--- Généré le : jeu. 17 nov. 2022 à 11:17
--- Version du serveur : 10.6.10-MariaDB-log
--- Version de PHP : 8.0.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 18, 2022 at 08:42 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,20 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cotes`
+-- Table structure for table `cotes`
 --
 
-CREATE TABLE `cotes` (
+DROP TABLE IF EXISTS `cotes`;
+CREATE TABLE IF NOT EXISTS `cotes` (
   `id` int(3) NOT NULL,
-  `cote_home` int(3) NOT NULL,
-  `cote_away` int(3) NOT NULL,
-  `egalite` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `cote_home` float NOT NULL,
+  `cote_away` float NOT NULL,
+  `egalite` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cotes`
+--
+
+INSERT INTO `cotes` (`id`, `cote_home`, `cote_away`, `egalite`) VALUES
+(1, 3.25, 3.1, 2.38),
+(2, 1.32, 4.85, 11.5),
+(3, 5.85, 3.9, 1.6),
+(4, 2.55, 3, 3.1),
+(5, 1.15, 7.85, 20),
+(6, 1.43, 4.4, 8),
+(7, 2.82, 3.05, 2.72),
+(8, 1.24, 6.5, 14),
+(9, 4.1, 3.3, 2),
+(10, 1.47, 4.55, 6.6),
+(11, 1.15, 7.3, 21),
+(12, 1.5, 4.35, 6.5),
+(13, 1.7, 3.55, 5.55),
+(14, 1.75, 3.45, 5.3),
+(15, 1.4, 4.55, 8.65),
+(16, 1.43, 4, 7.3),
+(17, 2.35, 3, 3.2),
+(18, 3.5, 3.2, 2.12),
+(19, 1.58, 3.85, 5.65),
+(20, 1.57, 3.8, 5.85),
+(21, 2.7, 3, 2.72),
+(22, 1.62, 3.65, 5.65),
+(23, 1.95, 3.45, 4.1),
+(24, 1.53, 5.85, 6.3),
+(25, 1.66, 3.55, 5.4),
+(26, 1.55, 4, 5.65),
+(27, 1.76, 3.6, 4.5),
+(28, 2.55, 3.3, 2.68),
+(29, 4.45, 3.45, 1.8),
+(30, 2.6, 2.95, 2.85),
+(31, 1.48, 4.2, 6.4),
+(32, 2.1, 3.2, 3.5),
+(33, 1.24, 5.7, 11.5),
+(34, 2.68, 3.15, 2.65),
+(35, 5.75, 3.85, 1.57),
+(36, 3.85, 3.3, 1.98),
+(37, 6.55, 4, 1.5),
+(38, 11.5, 5.55, 1.28),
+(39, 4.9, 3.8, 1.66),
+(40, 5.3, 3.6, 1.66),
+(41, 3, 3.2, 2.35),
+(42, 3.2, 3.3, 2.18),
+(43, 12, 5.7, 1.23),
+(44, 7.1, 4.6, 1.4),
+(45, 6.3, 4.2, 1.48),
+(46, 4.5, 3.5, 1.78),
+(47, 12, 5.65, 1.23),
+(48, 2.7, 3.2, 2.58);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `countries`
+-- Table structure for table `countries`
 --
 
 CREATE TABLE `countries` (
@@ -51,15 +106,17 @@ CREATE TABLE `countries` (
 -- Structure de la table `games`
 --
 
-CREATE TABLE `games` (
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE IF NOT EXISTS `games` (
   `match_id` varchar(2) NOT NULL,
   `round_number` varchar(300) DEFAULT NULL,
   `date` datetime(6) DEFAULT NULL,
   `location` varchar(300) DEFAULT NULL,
   `home_team` varchar(300) DEFAULT NULL,
   `away_team` varchar(300) DEFAULT NULL,
-  `group` varchar(300) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `group` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `games`
@@ -134,7 +191,7 @@ INSERT INTO `games` (`match_id`, `round_number`, `date`, `location`, `home_team`
 -- --------------------------------------------------------
 
 --
--- Structure de la table `predict`
+-- Table structure for table `predict`
 --
 
 CREATE TABLE `predict` (
@@ -159,13 +216,15 @@ INSERT INTO `predict` (`predict_ID`, `username`, `game_ID`, `score_home`, `score
 -- Structure de la table `score`
 --
 
-CREATE TABLE `score` (
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE IF NOT EXISTS `score` (
   `schoreh` int(5) NOT NULL,
   `scorea` int(5) NOT NULL,
-  `penalties` tinyint(1) NOT NULL DEFAULT 0,
-  `overtime` tinyint(1) NOT NULL DEFAULT 0,
-  `id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `penalties` tinyint(1) NOT NULL DEFAULT '0',
+  `overtime` tinyint(1) NOT NULL DEFAULT '0',
+  `id` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -173,13 +232,15 @@ CREATE TABLE `score` (
 -- Structure de la table `user`
 --
 
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(50) NOT NULL,
   `mail` varchar(50) NOT NULL,
   `mdp` varchar(200) NOT NULL,
-  `score` int(5) DEFAULT 10,
-  `mail_confirmed` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `score` int(5) DEFAULT '10',
+  `mail_confirmed` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
