16b25a9714c12b03e00aa8f9c1fbb8c2c6a7a741
martin <martin.labat92@gmail.com>
2022-11-20 13:32:02 +0100

    fix(sql): score in games
    Signed-off-by: martin <martin.labat92@gmail.com>

--- world_cup.sql
+++ world_cup.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 18, 2022 at 08:42 AM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: 10.10.10.30:3306
+-- Generation Time: Nov 20, 2022 at 01:28 PM
+-- Server version: 10.6.10-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `world_cup`
+-- Database: `worldcup`
 --
 
 -- --------------------------------------------------------
@@ -27,13 +27,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `cotes`
 --
 
-DROP TABLE IF EXISTS `cotes`;
-CREATE TABLE IF NOT EXISTS `cotes` (
+CREATE TABLE `cotes` (
   `id` int(3) NOT NULL,
   `cote_home` float NOT NULL,
   `cote_away` float NOT NULL,
   `egalite` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `cotes`
@@ -103,89 +102,90 @@ CREATE TABLE `countries` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `games`
+-- Table structure for table `games`
 --
 
 CREATE TABLE `games` (
-  `match_id` int(2) PRIMARY KEY NOT NULL AUTO_INCREMENT ,
+  `match_id` int(2) NOT NULL,
   `round_number` varchar(300) DEFAULT NULL,
   `date` datetime(6) DEFAULT NULL,
   `location` varchar(300) DEFAULT NULL,
   `home_team` varchar(300) DEFAULT NULL,
   `away_team` varchar(300) DEFAULT NULL,
   `group` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`match_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `home_team_score` int(11) DEFAULT NULL,
+  `away_team_score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `games`
+-- Dumping data for table `games`
 --
 
-INSERT INTO `games` (`match_id`, `round_number`, `date`, `location`, `home_team`, `away_team`, `group`) VALUES
-('1', '1', '2022-11-20 17:00:00.000000', 'Al Bayt Stadium', 'Qatar', 'Ecuador', 'Group A'),
-('10', '1', '2022-11-23 17:00:00.000000', 'Al Thumama Stadium', 'Spain', 'Costa Rica', 'Group E'),
-('11', '1', '2022-11-23 14:00:00.000000', 'Khalifa International Stadium', 'Germany', 'Japan', 'Group E'),
-('12', '1', '2022-11-23 11:00:00.000000', 'Al Bayt Stadium', 'Morocco', 'Croatia', 'Group F'),
-('13', '1', '2022-11-24 11:00:00.000000', 'Al Janoub Stadium', 'Switzerland', 'Cameroon', 'Group G'),
-('14', '1', '2022-11-24 14:00:00.000000', 'Education City Stadium', 'Uruguay', 'Korea Republic', 'Group H'),
-('15', '1', '2022-11-24 17:00:00.000000', 'Stadium 974', 'Portugal', 'Ghana', 'Group H'),
-('16', '1', '2022-11-24 20:00:00.000000', 'Lusail Stadium', 'Brazil', 'Serbia', 'Group G'),
-('17', '2', '2022-11-25 11:00:00.000000', 'Ahmad Bin Ali Stadium', 'Wales', 'Iran', 'Group B'),
-('18', '2', '2022-11-25 14:00:00.000000', 'Al Thumama Stadium', 'Qatar', 'Senegal', 'Group A'),
-('19', '2', '2022-11-25 17:00:00.000000', 'Khalifa International Stadium', 'Netherlands', 'Ecuador', 'Group A'),
-('2', '1', '2022-11-21 17:00:00.000000', 'Al Thumama Stadium', 'Senegal', 'Netherlands', 'Group A'),
-('20', '2', '2022-11-25 20:00:00.000000', 'Al Bayt Stadium', 'England', 'USA', 'Group B'),
-('21', '2', '2022-11-26 11:00:00.000000', 'Al Janoub Stadium', 'Tunisia', 'Australia', 'Group D'),
-('22', '2', '2022-11-26 14:00:00.000000', 'Education City Stadium', 'Poland', 'Saudi Arabia', 'Group C'),
-('23', '2', '2022-11-26 17:00:00.000000', 'Stadium 974', 'France', 'Denmark', 'Group D'),
-('24', '2', '2022-11-26 20:00:00.000000', 'Lusail Stadium', 'Argentina', 'Mexico', 'Group C'),
-('25', '2', '2022-11-27 11:00:00.000000', 'Ahmad Bin Ali Stadium', 'Japan', 'Costa Rica', 'Group E'),
-('26', '2', '2022-11-27 14:00:00.000000', 'Al Thumama Stadium', 'Belgium', 'Morocco', 'Group F'),
-('27', '2', '2022-11-27 17:00:00.000000', 'Khalifa International Stadium', 'Croatia', 'Canada', 'Group F'),
-('28', '2', '2022-11-27 20:00:00.000000', 'Al Bayt Stadium', 'Spain', 'Germany', 'Group E'),
-('29', '2', '2022-11-28 11:00:00.000000', 'Al Janoub Stadium', 'Cameroon', 'Serbia', 'Group G'),
-('3', '1', '2022-11-21 14:00:00.000000', 'Khalifa International Stadium', 'England', 'Iran', 'Group B'),
-('30', '2', '2022-11-28 14:00:00.000000', 'Education City Stadium', 'Korea Republic', 'Ghana', 'Group H'),
-('31', '2', '2022-11-28 17:00:00.000000', 'Stadium 974', 'Brazil', 'Switzerland', 'Group G'),
-('32', '2', '2022-11-28 20:00:00.000000', 'Lusail Stadium', 'Portugal', 'Uruguay', 'Group H'),
-('33', '3', '2022-11-29 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'Wales', 'England', 'Group B'),
-('34', '3', '2022-11-29 20:00:00.000000', 'Al Thumama Stadium', 'Iran', 'USA', 'Group B'),
-('35', '3', '2022-11-29 16:00:00.000000', 'Khalifa International Stadium', 'Ecuador', 'Senegal', 'Group A'),
-('36', '3', '2022-11-29 16:00:00.000000', 'Al Bayt Stadium', 'Netherlands', 'Qatar', 'Group A'),
-('37', '3', '2022-11-30 16:00:00.000000', 'Al Janoub Stadium', 'Australia', 'Denmark', 'Group D'),
-('38', '3', '2022-11-30 16:00:00.000000', 'Education City Stadium', 'Tunisia', 'France', 'Group D'),
-('39', '3', '2022-11-30 20:00:00.000000', 'Stadium 974', 'Poland', 'Argentina', 'Group C'),
-('4', '1', '2022-11-21 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'USA', 'Wales', 'Group B'),
-('40', '3', '2022-11-30 20:00:00.000000', 'Lusail Stadium', 'Saudi Arabia', 'Mexico', 'Group C'),
-('41', '3', '2022-12-01 16:00:00.000000', 'Ahmad Bin Ali Stadium', 'Croatia', 'Belgium', 'Group F'),
-('42', '3', '2022-12-01 16:00:00.000000', 'Al Thumama Stadium', 'Canada', 'Morocco', 'Group F'),
-('43', '3', '2022-12-01 20:00:00.000000', 'Khalifa International Stadium', 'Japan', 'Spain', 'Group E'),
-('44', '3', '2022-12-01 20:00:00.000000', 'Al Bayt Stadium', 'Costa Rica', 'Germany', 'Group E'),
-('45', '3', '2022-12-02 16:00:00.000000', 'Al Janoub Stadium', 'Ghana', 'Uruguay', 'Group H'),
-('46', '3', '2022-12-02 16:00:00.000000', 'Education City Stadium', 'Korea Republic', 'Portugal', 'Group H'),
-('47', '3', '2022-12-02 20:00:00.000000', 'Stadium 974', 'Serbia', 'Switzerland', 'Group G'),
-('48', '3', '2022-12-02 20:00:00.000000', 'Lusail Stadium', 'Cameroon', 'Brazil', 'Group G'),
-('49', 'Round of 16', '2022-12-03 16:00:00.000000', 'TBA', '1A', '2B', ''),
-('5', '1', '2022-11-22 20:00:00.000000', 'Al Janoub Stadium', 'France', 'Australia', 'Group D'),
-('50', 'Round of 16', '2022-12-03 20:00:00.000000', 'TBA', '1C', '2D', ''),
-('51', 'Round of 16', '2022-12-04 20:00:00.000000', 'TBA', '1B', '2A', ''),
-('52', 'Round of 16', '2022-12-04 16:00:00.000000', 'TBA', '1D', '2C', ''),
-('53', 'Round of 16', '2022-12-05 16:00:00.000000', 'TBA', '1E', '2F', ''),
-('54', 'Round of 16', '2022-12-05 20:00:00.000000', 'TBA', '1G', '2H', ''),
-('55', 'Round of 16', '2022-12-06 16:00:00.000000', 'TBA', '1F', '2E', ''),
-('56', 'Round of 16', '2022-12-06 20:00:00.000000', 'TBA', '1H', '2G', ''),
-('57', 'Quarter Finals', '2022-12-09 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('58', 'Quarter Finals', '2022-12-09 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('59', 'Quarter Finals', '2022-12-10 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('6', '1', '2022-11-22 14:00:00.000000', 'Education City Stadium', 'Denmark', 'Tunisia', 'Group D'),
-('60', 'Quarter Finals', '2022-12-10 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('61', 'Semi Finals', '2022-12-13 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('62', 'Semi Finals', '2022-12-14 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('63', 'Finals', '2022-12-17 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('64', 'Finals', '2022-12-18 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', ''),
-('7', '1', '2022-11-22 17:00:00.000000', 'Stadium 974', 'Mexico', 'Poland', 'Group C'),
-('8', '1', '2022-11-22 11:00:00.000000', 'Lusail Stadium', 'Argentina', 'Saudi Arabia', 'Group C'),
-('9', '1', '2022-11-23 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'Belgium', 'Canada', 'Group F');
+INSERT INTO `games` (`match_id`, `round_number`, `date`, `location`, `home_team`, `away_team`, `group`, `home_team_score`, `away_team_score`) VALUES
+(1, '1', '2022-11-20 17:00:00.000000', 'Al Bayt Stadium', 'Qatar', 'Ecuador', 'Group A', NULL, NULL),
+(2, '1', '2022-11-21 17:00:00.000000', 'Al Thumama Stadium', 'Senegal', 'Netherlands', 'Group A', NULL, NULL),
+(3, '1', '2022-11-21 14:00:00.000000', 'Khalifa International Stadium', 'England', 'Iran', 'Group B', NULL, NULL),
+(4, '1', '2022-11-21 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'USA', 'Wales', 'Group B', NULL, NULL),
+(5, '1', '2022-11-22 20:00:00.000000', 'Al Janoub Stadium', 'France', 'Australia', 'Group D', NULL, NULL),
+(6, '1', '2022-11-22 14:00:00.000000', 'Education City Stadium', 'Denmark', 'Tunisia', 'Group D', NULL, NULL),
+(7, '1', '2022-11-22 17:00:00.000000', 'Stadium 974', 'Mexico', 'Poland', 'Group C', NULL, NULL),
+(8, '1', '2022-11-22 11:00:00.000000', 'Lusail Stadium', 'Argentina', 'Saudi Arabia', 'Group C', NULL, NULL),
+(9, '1', '2022-11-23 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'Belgium', 'Canada', 'Group F', NULL, NULL),
+(10, '1', '2022-11-23 17:00:00.000000', 'Al Thumama Stadium', 'Spain', 'Costa Rica', 'Group E', NULL, NULL),
+(11, '1', '2022-11-23 14:00:00.000000', 'Khalifa International Stadium', 'Germany', 'Japan', 'Group E', NULL, NULL),
+(12, '1', '2022-11-23 11:00:00.000000', 'Al Bayt Stadium', 'Morocco', 'Croatia', 'Group F', NULL, NULL),
+(13, '1', '2022-11-24 11:00:00.000000', 'Al Janoub Stadium', 'Switzerland', 'Cameroon', 'Group G', NULL, NULL),
+(14, '1', '2022-11-24 14:00:00.000000', 'Education City Stadium', 'Uruguay', 'Korea Republic', 'Group H', NULL, NULL),
+(15, '1', '2022-11-24 17:00:00.000000', 'Stadium 974', 'Portugal', 'Ghana', 'Group H', NULL, NULL),
+(16, '1', '2022-11-24 20:00:00.000000', 'Lusail Stadium', 'Brazil', 'Serbia', 'Group G', NULL, NULL),
+(17, '2', '2022-11-25 11:00:00.000000', 'Ahmad Bin Ali Stadium', 'Wales', 'Iran', 'Group B', NULL, NULL),
+(18, '2', '2022-11-25 14:00:00.000000', 'Al Thumama Stadium', 'Qatar', 'Senegal', 'Group A', NULL, NULL),
+(19, '2', '2022-11-25 17:00:00.000000', 'Khalifa International Stadium', 'Netherlands', 'Ecuador', 'Group A', NULL, NULL),
+(20, '2', '2022-11-25 20:00:00.000000', 'Al Bayt Stadium', 'England', 'USA', 'Group B', NULL, NULL),
+(21, '2', '2022-11-26 11:00:00.000000', 'Al Janoub Stadium', 'Tunisia', 'Australia', 'Group D', NULL, NULL),
+(22, '2', '2022-11-26 14:00:00.000000', 'Education City Stadium', 'Poland', 'Saudi Arabia', 'Group C', NULL, NULL),
+(23, '2', '2022-11-26 17:00:00.000000', 'Stadium 974', 'France', 'Denmark', 'Group D', NULL, NULL),
+(24, '2', '2022-11-26 20:00:00.000000', 'Lusail Stadium', 'Argentina', 'Mexico', 'Group C', NULL, NULL),
+(25, '2', '2022-11-27 11:00:00.000000', 'Ahmad Bin Ali Stadium', 'Japan', 'Costa Rica', 'Group E', NULL, NULL),
+(26, '2', '2022-11-27 14:00:00.000000', 'Al Thumama Stadium', 'Belgium', 'Morocco', 'Group F', NULL, NULL),
+(27, '2', '2022-11-27 17:00:00.000000', 'Khalifa International Stadium', 'Croatia', 'Canada', 'Group F', NULL, NULL),
+(28, '2', '2022-11-27 20:00:00.000000', 'Al Bayt Stadium', 'Spain', 'Germany', 'Group E', NULL, NULL),
+(29, '2', '2022-11-28 11:00:00.000000', 'Al Janoub Stadium', 'Cameroon', 'Serbia', 'Group G', NULL, NULL),
+(30, '2', '2022-11-28 14:00:00.000000', 'Education City Stadium', 'Korea Republic', 'Ghana', 'Group H', NULL, NULL),
+(31, '2', '2022-11-28 17:00:00.000000', 'Stadium 974', 'Brazil', 'Switzerland', 'Group G', NULL, NULL),
+(32, '2', '2022-11-28 20:00:00.000000', 'Lusail Stadium', 'Portugal', 'Uruguay', 'Group H', NULL, NULL),
+(33, '3', '2022-11-29 20:00:00.000000', 'Ahmad Bin Ali Stadium', 'Wales', 'England', 'Group B', NULL, NULL),
+(34, '3', '2022-11-29 20:00:00.000000', 'Al Thumama Stadium', 'Iran', 'USA', 'Group B', NULL, NULL),
+(35, '3', '2022-11-29 16:00:00.000000', 'Khalifa International Stadium', 'Ecuador', 'Senegal', 'Group A', NULL, NULL),
+(36, '3', '2022-11-29 16:00:00.000000', 'Al Bayt Stadium', 'Netherlands', 'Qatar', 'Group A', NULL, NULL),
+(37, '3', '2022-11-30 16:00:00.000000', 'Al Janoub Stadium', 'Australia', 'Denmark', 'Group D', NULL, NULL),
+(38, '3', '2022-11-30 16:00:00.000000', 'Education City Stadium', 'Tunisia', 'France', 'Group D', NULL, NULL),
+(39, '3', '2022-11-30 20:00:00.000000', 'Stadium 974', 'Poland', 'Argentina', 'Group C', NULL, NULL),
+(40, '3', '2022-11-30 20:00:00.000000', 'Lusail Stadium', 'Saudi Arabia', 'Mexico', 'Group C', NULL, NULL),
+(41, '3', '2022-12-01 16:00:00.000000', 'Ahmad Bin Ali Stadium', 'Croatia', 'Belgium', 'Group F', NULL, NULL),
+(42, '3', '2022-12-01 16:00:00.000000', 'Al Thumama Stadium', 'Canada', 'Morocco', 'Group F', NULL, NULL),
+(43, '3', '2022-12-01 20:00:00.000000', 'Khalifa International Stadium', 'Japan', 'Spain', 'Group E', NULL, NULL),
+(44, '3', '2022-12-01 20:00:00.000000', 'Al Bayt Stadium', 'Costa Rica', 'Germany', 'Group E', NULL, NULL),
+(45, '3', '2022-12-02 16:00:00.000000', 'Al Janoub Stadium', 'Ghana', 'Uruguay', 'Group H', NULL, NULL),
+(46, '3', '2022-12-02 16:00:00.000000', 'Education City Stadium', 'Korea Republic', 'Portugal', 'Group H', NULL, NULL),
+(47, '3', '2022-12-02 20:00:00.000000', 'Stadium 974', 'Serbia', 'Switzerland', 'Group G', NULL, NULL),
+(48, '3', '2022-12-02 20:00:00.000000', 'Lusail Stadium', 'Cameroon', 'Brazil', 'Group G', NULL, NULL),
+(49, 'Round of 16', '2022-12-03 16:00:00.000000', 'TBA', '1A', '2B', '', NULL, NULL),
+(50, 'Round of 16', '2022-12-03 20:00:00.000000', 'TBA', '1C', '2D', '', NULL, NULL),
+(51, 'Round of 16', '2022-12-04 20:00:00.000000', 'TBA', '1B', '2A', '', NULL, NULL),
+(52, 'Round of 16', '2022-12-04 16:00:00.000000', 'TBA', '1D', '2C', '', NULL, NULL),
+(53, 'Round of 16', '2022-12-05 16:00:00.000000', 'TBA', '1E', '2F', '', NULL, NULL),
+(54, 'Round of 16', '2022-12-05 20:00:00.000000', 'TBA', '1G', '2H', '', NULL, NULL),
+(55, 'Round of 16', '2022-12-06 16:00:00.000000', 'TBA', '1F', '2E', '', NULL, NULL),
+(56, 'Round of 16', '2022-12-06 20:00:00.000000', 'TBA', '1H', '2G', '', NULL, NULL),
+(57, 'Quarter Finals', '2022-12-09 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(58, 'Quarter Finals', '2022-12-09 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(59, 'Quarter Finals', '2022-12-10 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(60, 'Quarter Finals', '2022-12-10 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(61, 'Semi Finals', '2022-12-13 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(62, 'Semi Finals', '2022-12-14 20:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(63, 'Finals', '2022-12-17 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL),
+(64, 'Finals', '2022-12-18 16:00:00.000000', 'TBA', 'To be announced', 'To be announced', '', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -202,7 +202,7 @@ CREATE TABLE `predict` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `predict`
+-- Dumping data for table `predict`
 --
 
 INSERT INTO `predict` (`predict_ID`, `username`, `game_ID`, `score_home`, `score_away`) VALUES
@@ -212,83 +212,80 @@ INSERT INTO `predict` (`predict_ID`, `username`, `game_ID`, `score_home`, `score
 -- --------------------------------------------------------
 
 --
--- Structure de la table `score`
+-- Table structure for table `score`
 --
 
-DROP TABLE IF EXISTS `score`;
-CREATE TABLE IF NOT EXISTS `score` (
+CREATE TABLE `score` (
   `schoreh` int(5) NOT NULL,
   `scorea` int(5) NOT NULL,
-  `penalties` tinyint(1) NOT NULL DEFAULT '0',
-  `overtime` tinyint(1) NOT NULL DEFAULT '0',
-  `id` int(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `penalties` tinyint(1) NOT NULL DEFAULT 0,
+  `overtime` tinyint(1) NOT NULL DEFAULT 0,
+  `id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `mail` varchar(50) NOT NULL,
   `mdp` varchar(200) NOT NULL,
   `score` int(5) DEFAULT 10,
-  `mail_confirmed` tinyint(1) DEFAULT 0
-    `is_admin` tinyint(1) DEFAULT 0
+  `mail_confirmed` tinyint(1) DEFAULT 0,
+  `is_admin` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `mail`, `mdp`, `score`, `mail_confirmed`) VALUES
-('userTest', 'user.test@edu.ece.fr', 'password', 0, 1);
+INSERT INTO `user` (`username`, `mail`, `mdp`, `score`, `mail_confirmed`, `is_admin`) VALUES
+('userTest', 'user.test@edu.ece.fr', 'password', 0, 1, 0);
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `games`
+-- Indexes for table `games`
 --
 ALTER TABLE `games`
   ADD PRIMARY KEY (`match_id`);
 
 --
--- Index pour la table `predict`
+-- Indexes for table `predict`
 --
 ALTER TABLE `predict`
   ADD PRIMARY KEY (`predict_ID`);
 
 --
--- Index pour la table `score`
+-- Indexes for table `score`
 --
 ALTER TABLE `score`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `predict`
+-- AUTO_INCREMENT for table `games`
 --
 ALTER TABLE `games`
-  MODIFY `match_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+  MODIFY `match_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
-
-
+--
+-- AUTO_INCREMENT for table `predict`
+--
 ALTER TABLE `predict`
   MODIFY `predict_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
